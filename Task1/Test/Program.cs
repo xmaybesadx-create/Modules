e3d53fabfa431cf307c2c1e8dbcf8fa528a919d4
KiEROKi <xmaybesadx@gmail.com>
2026-03-25 13:53:14 +0200

    Add files via upload

---
+++ Task1/Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextProcessor processor = new TextProcessor();
+
+            string inputFile = @"E:\Visual Studio Insader\Projects\Test\textPD21.txt";
+            string outputFile = @"E:\Visual Studio Insader\Projects\Test\resultPD21.txt";
+
+            File.AppendAllText(outputFile, "=== UPPERCASE ===\n");
+            processor.ProcessFile(inputFile, outputFile, processor.ToUpperCase);
+            File.AppendAllText(outputFile, "\n=== CHAR COUNT ===\n");
+            processor.ProcessFile(inputFile, outputFile, processor.CountChars);
+            File.AppendAllText(outputFile, "\n=== WORD COUNT ===\n");
+            processor.ProcessFile(inputFile, outputFile, processor.CountWords);
+        }
+    }
+}
---
+++ Task1/Test/TextProcessor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Test
+{
+    public class TextProcessor
+    {
+        public delegate string TextOperation(string text);
+
+        public void ProcessFile(string inputPath, string outputPath, TextOperation operation)
+        {
+            string[] lines = File.ReadAllLines(inputPath);
+            foreach (string line in lines)
+            {
+                string result = operation(line);
+                File.AppendAllText(outputPath, result + "\n");
+            }
+        }
+        public string ToUpperCase(string text)
+        {
+            return text.ToUpper();
+        }
+        public string CountChars(string text)
+        {
+            return text.Length.ToString();
+        }
+        public string CountWords(string text)
+        {
+            return text.Split(' ', StringSplitOptions.RemoveEmptyEntries).Length.ToString();
+        }
+    }
+}
